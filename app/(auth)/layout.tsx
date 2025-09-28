f10354c464842a89efc19ab5544417d447c7a9bf
Aditya <102992848+addymistrel@users.noreply.github.com>
2025-09-28 23:19:59 +0530

    Implement code changes to enhance functionality and improve performance

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -42,11 +42,17 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
               ))}
             </div>
           </div>
-              </div>
-              
-              <div className="flex-1 relative">
-                  <Image src={"/assets/images/dashboard.png"} alt="Dashboard Preview" width={1440} height={1150} className="auth-dashboard-preview absolute top-0" />
-              </div>
+        </div>
+
+        <div className="flex-1 relative">
+          <Image
+            src={"/assets/images/dashboard.png"}
+            alt="Dashboard Preview"
+            width={1440}
+            height={1150}
+            className="auth-dashboard-preview absolute top-0"
+          />
+        </div>
       </section>
     </main>
   );
